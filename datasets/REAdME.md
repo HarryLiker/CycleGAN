ce953ae9f5b8418551ce87dfb5088a01aff611c9
HarryLiker <ikorolew02@mail.ru>
2023-06-30 14:05:30 +0300

    Внесены небольшие исправления

--- datasets/REAdME.md
+++ datasets/REAdME.md
@@ -1,9 +1,7 @@
 # Установка датасетов
 
-Датасет **horse2zebra** можно установить по [ссылке](https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/horse2zebra.zip).
+Датасет **horse2zebra** можно установить по [ссылке на датасет](https://people.eecs.berkeley.edu/~taesung_park/CycleGAN/datasets/horse2zebra.zip) или из [google-диска](https://drive.google.com/drive/folders/1AlitKBFAT7c8C7-ZbAGSI89ihELniJtJ?usp=drive_link)
 
-Датасет **Forest Fire** можно установить по [ссылке](https://www.kaggle.com/datasets/alik05/forest-fire-dataset).
-
-Содержимое датасета **Forest Fire** нужно распределить по папкам для обучения модели CycleGAN на нем. Датасет с распределенными данными можно установить по [ссылке]().
+Датасет **Forest Fire** можно установить по [ссылке](https://www.kaggle.com/datasets/alik05/forest-fire-dataset) или из [google-диска](https://drive.google.com/drive/folders/1AlitKBFAT7c8C7-ZbAGSI89ihELniJtJ?usp=drive_link), где содержимое датасета уже распределено по папкам для обучения модели.
 
 Для обучения модели на датасете, его необходимо распаковать в папку ```datasets```.
--- utils/arg_parser.py
+++ utils/arg_parser.py
@@ -9,15 +9,15 @@ def train_parse():
     parser.add_argument('--lr',                         type=float,     default=0.0002,             help='Learning rate. Default = 0.0002')
     parser.add_argument('--num_workers',                type=int,       default=2,                  help='Number of subprocesses to use for data loading. Default = 2')
     parser.add_argument('--batch_size',                 type=int,       default=1,                  help='Size of batch with images. Default = 1')
-    parser.add_argument('--device',                     type=str,       default='cuda',             help='Device on which calculations will be performed')
-    parser.add_argument('--unaligned',                  type=bool,      default=False,              help='Attachment of indexes of images of different types to each other')
+    parser.add_argument('--device',                     type=str,       default='cuda',             help='Device on which calculations will be performed. Default = cuda')
+    parser.add_argument('--unaligned',                  type=bool,      default=False,              help='Attachment of indexes of images of different types to each other. Default = False')
     parser.add_argument('--img_size',                   type=int,       default=256,                help='Size of image to be converted. Default = 256')
     parser.add_argument('--lambda_value',               type=float,     default=10.0,               help='Value of lambda. Default = 10.0')
     parser.add_argument('--datasets_dir',               type=str,       default='./datasets',       help='Directory where datasets are located. Default = ./datasets')
     parser.add_argument('--dataset_name',               type=str,       default='horse2zebra',      help='Name of the dataset to work with. Default = horse2zebras')
     parser.add_argument('--train_hist_dir',             type=str,       default='./train_history',  help='Directory with training history for different datasets')
     parser.add_argument('--n_imgs_save',                type=int,       default=0,                  help='The number of test images to save at the epoch with results. Default = 0')
-    parser.add_argument('--weights_dir',                type=str,       default='./weights',        help='Directory from where you can download or save the weights of various datasets')
+    parser.add_argument('--weights_dir',                type=str,       default='./weights',        help='Directory from where you can download or save the weights of various datasets. Default = ./weights')
 
     args = parser.parse_args()
 
@@ -42,9 +42,9 @@ def eval_parse():
     parser = argparse.ArgumentParser()
 
     # Ключи для ввода при запуске программы для вычисления изображений
-    parser.add_argument('--imgs_dir',           type=str,       default='./imgs_to_eval',           help='Directory of images to evaluate. Default=./imgs_to_eval')
+    parser.add_argument('--imgs_dir',           type=str,       default='./imgs_to_eval',           help='Directory of images to evaluate. Default = ./imgs_to_eval')
     parser.add_argument('--device',             type=str,       default='cuda',                     help='Device on which calculations will be performed. Default = cuda')
-    parser.add_argument('--results_path',       type=str,       default='./eval_results',           help='Path to result of evaluating')
+    parser.add_argument('--results_path',       type=str,       default='./eval_results',           help='Path to result of evaluating. Default = ./eval_results')
     parser.add_argument('--weights_path',       type=str,       default='./weights/horse2zebra/',   help='Path to pretrained model weights with two generators and two discriminators. Default = ./weights/horse2zebra')
 
     args = parser.parse_args()
